12c11cc6763b309bc86b0c1a38e0d6157b0337db
Anton Ovchinnikov <anton@tonyo.info>
2018-09-11 15:57:06 +0200

    deploy: Fetch the data on container start-up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -ex
+set -e
 
-if [ $1 = 'bash' ]; then
+if [ "$1" == 'bash' ]; then
   exec bash
-fi
-
-if [ "$1" == 'run' ]; then
+elif [ "$1" == 'run' ]; then
   BASE_CMD="uwsgi --master --manage-script-name --wsgi apiserver:app"
   DEFAULT_ARGS="--socket /tmp/registry.sock --http 0.0.0.0:5030 --workers=2 --max-requests=10000"
   if [ "$#" -gt 1 ]; then
@@ -14,6 +12,13 @@ if [ "$1" == 'run' ]; then
   else
     echo "Running with default arguments: ${DEFAULT_ARGS}"
     set -- ${BASE_CMD} ${DEFAULT_ARGS}
+  fi
+
+  # Init the registry if the data directory does not exist or empty
+  REGISTRY_DIR=".registry"
+  if [ ! -d "${REGISTRY_DIR}" ] || [ -z "$(ls -A ${REGISTRY_DIR})" ]; then
+    echo 'Fetching the data for the first time...'
+    gosu registry flask update-repo
   fi
 elif [ "$1" == 'update-repo' ]; then
   set -- flask update-repo
