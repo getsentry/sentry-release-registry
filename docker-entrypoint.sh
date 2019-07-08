1694781709bf7365b788eeede0a45681526227a9
Matt Robenolt <matt@ydekproductions.com>
2019-07-07 21:38:34 -0700

    Switch to mywsgi for running
    Also a few other better practices

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ "$1" == 'bash' ]; then
-  exec bash
-elif [ "$1" == 'run' ]; then
-  BASE_CMD="uwsgi --master --manage-script-name --wsgi apiserver:app"
-  DEFAULT_ARGS="--socket /tmp/registry.sock --http 0.0.0.0:5030 --workers=2 --max-requests=10000"
-  if [ "$#" -gt 1 ]; then
-    echo "Running with arguments:" "${@:2}"
-    set -- ${BASE_CMD} ${@:2}
-  else
-    echo "Running with default arguments: ${DEFAULT_ARGS}"
-    set -- ${BASE_CMD} ${DEFAULT_ARGS}
-  fi
-else
-  echo 'Commands: run'
-  exit 1
+if [ "$1" = 'mywsgi' ]; then
+  set -- gosu registry "$@"
 fi
 
-exec gosu registry "$@"
+exec "$@"
