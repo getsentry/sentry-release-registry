1f118ddc39c33f4c989aa40ba4bacf8d6e19e8e7
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-17 11:30:30 +0200

    add caching

--- api-node/src/app.module.ts
+++ api-node/src/app.module.ts
@@ -7,9 +7,15 @@ import { SdksController } from './sdks/sdks.controller';
 import { AwsLambdaLayersController } from './aws-lambda-layers/aws-lambda-layers.controller';
 import { RegistryService } from './common/registry.service';
 import { SentryGlobalFilter, SentryModule } from '@sentry/nestjs/setup';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 @Module({
-  imports: [SentryModule.forRoot()],
+  imports: [
+    SentryModule.forRoot(),
+    // max and ttl taken from apiserver.py cache config
+    // ttl of cache-manager@5 is in milliseconds
+    CacheModule.register({ max: 200, ttl: 3600 * 1000 }),
+  ],
   controllers: [
     HealthCheckController,
     PackagesController,
@@ -21,6 +27,10 @@ import { APP_FILTER } from '@nestjs/core';
   providers: [
     RegistryService,
     { provide: APP_FILTER, useClass: SentryGlobalFilter },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule {}
