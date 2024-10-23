c216ccac72f52545a755124629f65969d3ffab92
Lukas Stracke <lukas.stracke@sentry.io>
2024-10-23 14:00:03 +0200

    add cors

--- api-node/src/main.ts
+++ api-node/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'fatal', 'verbose', 'log', 'warn'],
   });
+  app.enableCors({ origin: '*' });
   await app.listen(3000);
 }
 
