a1687427e21f4f5408e5b74d2458d30af466dd0c
Lukas Stracke <lukas.stracke@sentry.io>
2024-11-04 13:07:56 +0100

    dynamic port

--- api-node/src/main.ts
+++ api-node/src/main.ts
@@ -4,6 +4,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AlignHeadersInterceptor } from './common/alignHeaders.interceptor';
 
+const DEFAULT_PORT = 3000;
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: ['debug', 'error', 'fatal', 'verbose', 'log', 'warn'],
@@ -16,7 +18,18 @@ async function bootstrap(): Promise<void> {
   // disable setting the etag header
   app.getHttpAdapter().getInstance().set('etag', false);
 
-  await app.listen(3000);
+  await app.listen(getPort());
+}
+
+function getPort(): number {
+  try {
+    if (process.env.PORT) {
+      return parseInt(process.env.PORT);
+    }
+  } catch (error) {
+    console.error('Invalid port number', error);
+  }
+  return DEFAULT_PORT;
 }
 
 bootstrap();
